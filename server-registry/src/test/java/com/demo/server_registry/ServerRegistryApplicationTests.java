cb9869f4c56726adfb841c1f78d1285cb9297ea2
Soorya Thejus(UST <287591@ust.com>
2024-10-02 16:25:06 +0530

    first commit

---
+++ server-registry/src/test/java/com/demo/server_registry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.server_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
